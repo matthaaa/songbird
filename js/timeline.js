cd8357478232b508a6aa037cdffe91838fe70d33
Matt Ha <matthewha1993@gmail.com>
2018-02-15 11:12:41 -0800

    Implement playback feature for timeline

--- js/timeline.js
+++ js/timeline.js
@@ -1,4 +1,5 @@
 function timeline(keysByNoteName, selectedKeys) {
+  document.getElementById("playButton").onclick = function() {playback()};
   var timelineCanvas = document.getElementById("timelineCanvas");
   var ctx = timelineCanvas.getContext("2d")
 
@@ -17,4 +18,23 @@ function timeline(keysByNoteName, selectedKeys) {
     const note = keysByNoteName[key]
     drawCircle(key, note);
   })
+
+  function playback() {
+    keys = Array.from(selectedKeys);
+    playedNotes = [];
+
+    if (keys.length === 0) return null;
+
+    const timer = setInterval(() => {
+      playedNotes.push(keys.shift());
+
+      noteIdx = playedNotes.length - 1;
+      var audio = new Audio(keysByNoteName[playedNotes[noteIdx]].soundSrc);
+      audio.play();
+
+      if (keys.length === 0) {
+        clearInterval(timer, 0);
+      }
+    }, 500);
+  }
 }
