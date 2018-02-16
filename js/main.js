8b4fc7ededa5f9366abb08e1b133c120ccbcd78b
Matt Ha <matthewha1993@gmail.com>
2018-02-16 11:33:51 -0800

    Add test sounds for two C keys

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Frequencies of two octaves in the key of C.
 let keysByNoteName = {
-  0: {name: 'c', soundSrc: '../assets/c_scale_audio/c1.wav'},
+  0: {name: 'c', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/c1.wav'},
   1: {name: 'd', soundSrc: '../assets/c_scale_audio/d1.wav'},
   2: {name: 'e', soundSrc: '../assets/c_scale_audio/e1.wav'},
   3: {name: 'f', soundSrc: '../assets/c_scale_audio/f1.wav'},
   4: {name: 'g', soundSrc: '../assets/c_scale_audio/g1.wav'},
   5: {name: 'a', soundSrc: '../assets/c_scale_audio/a1.wav'},
   6: {name: 'b', soundSrc: '../assets/c_scale_audio/b1.wav'},
-  7: {name: 'c2', soundSrc: '../assets/c_scale_audio/c2.wav'},
+  7: {name: 'c2', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/c2.wav'},
   8: {name: 'd2', soundSrc: '../assets/c_scale_audio/d2.wav'},
   9: {name: 'e2', soundSrc: '../assets/c_scale_audio/e2.wav'},
   10: {name: 'f2', soundSrc: '../assets/c_scale_audio/f2.wav'},
