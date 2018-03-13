1c725066a7f3dd7e35dc2811c2cf33329ade2c37
Matt Ha <matthewha1993@gmail.com>
2018-03-13 15:33:51 -0700

    Change pitch names to follow scientific musical naming convention

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,19 +1,19 @@
 function keyboard(keysByNoteName, selectedKeys, nextKeys) {
-  document.getElementById("c").onclick = function() {playKey(0)};
-  document.getElementById("d").onclick = function() {playKey(1)};
-  document.getElementById("e").onclick = function() {playKey(2)};
-  document.getElementById("f").onclick = function() {playKey(3)};
-  document.getElementById("g").onclick = function() {playKey(4)};
-  document.getElementById("a").onclick = function() {playKey(5)};
-  document.getElementById("b").onclick = function() {playKey(6)};
-  document.getElementById("c2").onclick = function() {playKey(7)};
-  document.getElementById("d2").onclick = function() {playKey(8)};
-  document.getElementById("e2").onclick = function() {playKey(9)};
-  document.getElementById("f2").onclick = function() {playKey(10)};
-  document.getElementById("g2").onclick = function() {playKey(11)};
-  document.getElementById("a2").onclick = function() {playKey(12)};
-  document.getElementById("b2").onclick = function() {playKey(13)};
-  document.getElementById("c3").onclick = function() {playKey(14)};
+  document.getElementById("c3").onclick = function() {playKey(0)};
+  document.getElementById("d3").onclick = function() {playKey(1)};
+  document.getElementById("e3").onclick = function() {playKey(2)};
+  document.getElementById("f3").onclick = function() {playKey(3)};
+  document.getElementById("g3").onclick = function() {playKey(4)};
+  document.getElementById("a4").onclick = function() {playKey(5)};
+  document.getElementById("b4").onclick = function() {playKey(6)};
+  document.getElementById("c4").onclick = function() {playKey(7)};
+  document.getElementById("d4").onclick = function() {playKey(8)};
+  document.getElementById("e4").onclick = function() {playKey(9)};
+  document.getElementById("f4").onclick = function() {playKey(10)};
+  document.getElementById("g4").onclick = function() {playKey(11)};
+  document.getElementById("a5").onclick = function() {playKey(12)};
+  document.getElementById("b5").onclick = function() {playKey(13)};
+  document.getElementById("c5").onclick = function() {playKey(14)};
 
   document.getElementById("clear").onclick = function() {clearKeys()};
 
