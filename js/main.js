36aae992ad148dcbc080c5b6c6c1ce885d087173
Matt <matthewha1993@gmail.com>
2018-08-23 17:11:45 -0700

    Create init function

--- js/main.js
+++ js/main.js
@@ -2,27 +2,33 @@
 // TODO: Initialize using JavaScript best practices
 // **************************************************
 
-// Frequencies of two octaves in the key of C.
-let keysByNoteName = {
-  0: {name: 'c3', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/c1.wav'},
-  1: {name: 'd3', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/d1.wav'},
-  2: {name: 'e3', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/e1.wav'},
-  3: {name: 'f3', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/f1.wav'},
-  4: {name: 'g3', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809536/g1.wav'},
-  5: {name: 'a4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/a1.wav'},
-  6: {name: 'b4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/b1.wav'},
-  7: {name: 'c4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/c2.wav'},
-  8: {name: 'd4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/d2.wav'},
-  9: {name: 'e4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/e2.wav'},
-  10: {name: 'f4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/f2.wav'},
-  11: {name: 'g4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/g2.wav'},
-  12: {name: 'a5', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/a2.wav'},
-  13: {name: 'b5', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/b2.wav'},
-  14: {name: 'c5', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/c.wav'},
-};
+const init = () => {
 
-let selectedKeys = [];
-let nextKeys = Object.keys(keysByNoteName).map(key => Number(key));
+  // Frequencies of two octaves in the key of C; key integers used to
+  // determine relative notes.
+  let keysByNoteName = {
+    0: {name: 'c3', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/c1.wav'},
+    1: {name: 'd3', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/d1.wav'},
+    2: {name: 'e3', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/e1.wav'},
+    3: {name: 'f3', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/f1.wav'},
+    4: {name: 'g3', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809536/g1.wav'},
+    5: {name: 'a4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/a1.wav'},
+    6: {name: 'b4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/b1.wav'},
+    7: {name: 'c4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/c2.wav'},
+    8: {name: 'd4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/d2.wav'},
+    9: {name: 'e4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/e2.wav'},
+    10: {name: 'f4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/f2.wav'},
+    11: {name: 'g4', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809535/g2.wav'},
+    12: {name: 'a5', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/a2.wav'},
+    13: {name: 'b5', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/b2.wav'},
+    14: {name: 'c5', soundSrc: 'http://res.cloudinary.com/instagraph/video/upload/v1518809534/c.wav'},
+  };
 
-new keyboard(keysByNoteName, selectedKeys, nextKeys)
-new timeline(keysByNoteName, selectedKeys);
+  let selectedKeys = [];
+  let nextKeys = Object.keys(keysByNoteName).map(key => Number(key));
+
+  new keyboard(keysByNoteName, selectedKeys, nextKeys)
+  new timeline(keysByNoteName, selectedKeys);
+}
+
+init();
